a218deb13572615c5413064be994b1533866bc83
Ramraghul <raghulraghul111@gmail.com>
2022-09-22 11:33:43 +0530

    DB cloud add

--- server.js
+++ server.js
@@ -11,6 +11,10 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const mongoDB = require("mongodb")
+const MongoClient = mongoDB.MongoClient
+const URL = "mongodb+srv://Ramcharan:ramcharan@cluster0.x5qkemg.mongodb.net/?retryWrites=true&w=majority"
+const DB = "API"
 
 // Middleware
 app.use(express.json())
@@ -39,24 +43,60 @@ let users=[];
 
 
 // POST method
-app.post("/post",function(req,res){
-    req.body.id = users.length + 1
-    users.push(req.body)
-    res.json({Message:"Sir you have a post"})
+app.post("/post",async function(req,res){
+    // req.body.id = users.length + 1
+    // users.push(req.body)
+    // res.json({Message:"Sir you have a post"})
+
+    try {
+        //1.Create a connection nodejs to mongoDB
+    const connection =await MongoClient.connect(URL)
+
+    //2.Select DB
+    const db = connection.db(DB)
+
+    //3.Select Collection
+    //4.C.R.E.D Function
+    await db.collection("users").insertOne(req.body)
+
+    //5.Close connection
+    await connection.close()
+
+    res.json({Message:"Done"})
+    } catch (error) {
+        res.status(500).json({Message:"something went wrong"})
+    }
 })
 
 //GET method Qurypara
-app.get("/users",function(req,res){
+app.get("/users", async function(req,res){
     // let qparms = req.query
     // console.log(qparms);
-    let  resUser=[];
-    for (let index = parseInt(req.query.offset); index < parseInt(req.query.offset) + parseInt(req.query.limit); index++) {
-       if(users[index]){
-        resUser.push(users[index])
-       } ;
-        
-    }
+    // let  resUser=[];
+    // for (let index = parseInt(req.query.offset); index < parseInt(req.query.offset) + parseInt(req.query.limit); index++) {
+    //    if(users[index]){
+    //     resUser.push(users[index])
+    //    } ;
+    // }
+    try {
+        //1.Create a connection nodejs to mongoDB
+    const connection =await MongoClient.connect(URL)
+
+     //2.Select DB
+     const db = connection.db(DB)
+
+     //3.Select Collection
+    //4.C.R.E.D Function
+    const resUser = await db.collection("users").find().toArray()
+
+    //5.Close connection
+    await connection.close()
+
     res.json(resUser)
+    } catch (error) {
+        res.status(500).json({Message:"something went wrong"})
+    }
+    
 })
 
 //GET all users
@@ -66,44 +106,99 @@ app.get("/users/all",function(req,res){
 
 
 //VIEW method
-app.get("/users/:id",function(req,res){
-    let userid = req.params.id;
-    let use = users.find((item)=> item.id == userid)  //{} Return key is must
-    if(use){
-        res.json(use)
-    }else{
-        res.json({Alert:"unakku oru thadava sonna puriyatha"})
+app.get("/users/:id",async function(req,res){
+    // let userid = req.params.id;
+    // let use = users.find((item)=> item.id == userid)  //{} Return key is must
+    // if(use){
+    //     res.json(use)
+    // }else{
+    //     res.json({Alert:"unakku oru thadava sonna puriyatha"})
+    // }
+    try {
+        //1.Create a connection nodejs to mongoDB
+    const connection =await MongoClient.connect(URL)
+
+     //2.Select DB
+     const db = connection.db(DB)
+
+     //3.Select Collection
+    //4.C.R.E.D Function
+    const parti = await db.collection("users").findOne({_id:mongoDB.ObjectId(req.params.id)})
+
+    //5.Close connection
+    await connection.close()
+
+    res.json(parti)
+    } catch (error) {
+        res.status(500).json({Message:"something went wrong"})
     }
 })
 
 //EDIT method
-app.put("/users/:id",function(req,res){
-    let userid = req.params.id;
-    let change = users.findIndex((item)=> item.id == userid);
+app.put("/users/:id",async function(req,res){
+    // let userid = req.params.id;
+    // let change = users.findIndex((item)=> item.id == userid);
 
-    if(change != -1){
-        Object.keys(req.body).forEach((item)=>{
-            users[change][item]= req.body[item]
-        })
-        res.json({EDITH:"Edit don sir"})
+    // if(change != -1){
+    //     Object.keys(req.body).forEach((item)=>{
+    //         users[change][item]= req.body[item]
+    //     })
+    //     res.json({EDITH:"Edit don sir"})
 
-    } else{
-        res.json({Error:"User illa da lusu"})
+    // } else{
+    //     res.json({Error:"User illa da lusu"})
+    // }
+    try {
+        //1.Create a connection nodejs to mongoDB
+    const connection =await MongoClient.connect(URL)
+
+     //2.Select DB
+     const db = connection.db(DB)
+
+     //3.Select Collection
+    //4.C.R.E.D Function
+    const parti = await db.collection("users").findOneAndUpdate({_id:mongoDB.ObjectId(req.params.id)},{$set:req.body})
+
+    //5.Close connection
+    await connection.close()
+
+    res.json(parti)
+    } catch (error) {
+        res.status(500).json({Message:"something went wrong"})
     }
-
-    //DELETE method
-    app.delete("/users/:id",function(req,res){
-        let userid = req.params.id;
-    let delet = users.findIndex((item)=> item.id == userid);
-
-    if(delet != -1){
-        users.splice(delet,1);
-        res.json({Message:"Katham margaya"})
-    }else{
-        res.json({Error:"Somthing wrong"})
-    }
-    })
 
 })
+
+  //DELETE method
+  app.delete("/users/:id",async function(req,res){
+//     let userid = req.params.id;
+// let delet = users.findIndex((item)=> item.id == userid);
+
+// if(delet != -1){
+//     users.splice(delet,1);
+//     res.json({Message:"Katham margaya"})
+// }else{
+//     res.json({Error:"Somthing wrong"})
+// }
+try {
+    //1.Create a connection nodejs to mongoDB
+const connection =await MongoClient.connect(URL)
+
+ //2.Select DB
+ const db = connection.db(DB)
+
+ //3.Select Collection
+//4.C.R.E.D Function
+await db.collection("users").findOneAndDelete({_id:mongoDB.ObjectId(req.params.id)})
+
+//5.Close connection
+await connection.close()
+
+res.json({message:"Delete data"})
+} catch (error) {
+    res.status(500).json({Message:"something went wrong"})
+}
+})
+
 
 app.listen(process.env.PORT || 3001)
